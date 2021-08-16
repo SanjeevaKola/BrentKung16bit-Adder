4c5193057e91e0e05fbd49cd7af178f4c841faa7
SanjeevaKola <88876251+SanjeevaKola@users.noreply.github.com>
2021-08-16 12:45:13 +0530

    Add files via upload

---
+++ BrentKung16bit_TBS.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+
+module BrentKung16bit_TBS;
+
+	// Inputs
+	reg [15:0] X;
+	reg [15:0] Y;
+	reg Cin;
+
+	// Outputs
+	wire Cout;
+	wire [15:0] SUM;
+
+	// Instantiate the Unit Under Test (UUT)
+	BrentKung16bit dut (
+		.a(X), 
+		.b(Y), 
+		.Cin(Cin), 
+		.Sum(SUM),
+		.Cout(Cout) 
+	);
+
+	initial begin
+		// Initialize Inputs
+		X = 0;
+		Y = 0;
+		Cin = 0;
+
+		#10;
+        X=22;
+		  Y=53;
+		  Cin=0;
+		  #10
+		   X=35;
+		  Y=42;
+		  Cin=1;
+		  
+		  		  #10
+		   X=243;
+		  Y=37;
+		  Cin=0;
+		  
+		  		  #10
+		   X=645;
+		  Y=246;
+		  Cin=0;
+
+		  #10
+		   X=7;
+		  Y=6;
+		  Cin=1;
+
+	end
+      
+endmodule
+
